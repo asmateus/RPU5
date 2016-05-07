2d14674b7fb126319f3e0a963f5b7c724e3dac48
Andrés Simancas Mateus <mateus@uninorte.edu.co>
2016-05-07 16:31:44 -0500

    Callbacks

---
+++ src/Callbacks.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * La idea es usar el patrón de diseño Proveedor-consumidor.
+ * Esta clase contiene las banderas que la GUI actualizará constantemente.  
+*/
+namespace RPU5
+{
+    class Callbacks
+    {
+    }
+}
